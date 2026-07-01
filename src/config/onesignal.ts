97073a3910ad47e95b2ea0b055c5500c9fd19306
n2pqktc625-stack <291402370+n2pqktc625-stack@users.noreply.github.com>
2026-07-01 23:46:40 +0100

    Delete src directory

--- src/config/onesignal.ts
+++
@@ -1,5 +0,0 @@
-export const ONESIGNAL_CONFIG = {
-  appId: process.env.ONESIGNAL_APP_ID,
-  restApiKey: process.env.ONESIGNAL_REST_API_KEY,
-  projectNumber: process.env.ONESIGNAL_PROJECT_NUMBER,
-}; 
